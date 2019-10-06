2a03c64446aa53bf21c2356a63e0c318f6b8ca1d
Pavan Prakash <pavan.prakash.contractor@move.com>
2019-10-06 18:23:43 +0530

    setup routing

--- app/javascript/packs/Home.jsx
+++ app/javascript/packs/Home.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { Link } from "@reach/router";
 
-const Home = () => <div>Hello!</div>;
+const Home = () => (
+  <div>
+    <Link to="/about">About</Link>
+    <Link to="/contact">Contact</Link>
+    Hello!
+  </div>
+);
 
 export default Home;
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static#index'
-  get 'home', to: 'static#index'
 
   constraints subdomain: 'api' do
     scope module: 'api' do
-      namespace :v1 do
+      namespace :v1, defaults: { format: 'json' } do
         # v1 routes go here example.com/v1/some-url
       end
     end
+  end
+
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
   end
 end
