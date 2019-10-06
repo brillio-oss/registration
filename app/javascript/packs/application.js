8920c47a0a091672dc532d390765a4c89f48e657
Pavan Prakash <pavan.prakash.contractor@move.com>
2019-10-06 17:41:11 +0530

    add basic page layout with router

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,19 +1,26 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router } from "@reach/router";
+
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
 
 require("@rails/ujs").start();
 require("@rails/activestorage").start();
 require("channels");
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-// Support component names relative to this directory:
 const componentRequireContext = require.context("components", true);
 const ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(
+    <Router>
+      <Home path="/" />
+      <About path="/about" />
+      <Contact path="/contact" />
+    </Router>,
+    document.body.appendChild(document.createElement("div"))
+  );
+});
